4ae52fa457f82ea9de019192b949c9254f3fe351
Afshan Firdose <Afshan Firdose@DESKTOP-S420HUE>
2021-08-04 13:40:47 +0530

    first commit

---
+++ SDET18Api-Framework/src/test/java/com/crm/sdet18Api/Demo.java
@@ -0,0 +1,13 @@
+package com.crm.sdet18Api;
+
+import org.testng.annotations.Test;
+
+public class Demo {
+	@Test
+	public void demoTest() {
+		System.out.println("hi there i am in api framework");
+	}
+	
+
+
+}
